9ed6a2d73a2f77e8e120c70b1e7de9a43c092f4d
seo kang chun <skc3779@gmail.com>
2014-11-28 11:10:20 +0900

    single web application with gradle tools

---
+++ src/main/java/com/web/configs/DomainConfiguration.java
@@ -0,0 +1,20 @@
+package com.web.configs;
+
+import com.web.services.HelloString;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by seokangchun on 14. 11. 28..
+ */
+@Configuration
+@PropertySource("classpath:connect.properties")
+@ComponentScan(basePackages = {"com.web.services"})
+public class DomainConfiguration {
+//    @Bean
+//    public static HelloString helloString() {
+//        return new HelloString();
+//    }
+}
